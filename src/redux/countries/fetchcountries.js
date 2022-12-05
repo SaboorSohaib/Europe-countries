a79b691cb44fb9ec78b1edde55f92ae843dfd788
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-12-05 20:41:48 +0430

    Add some library

---
+++ src/redux/countries/fetchcountries.js
@@ -0,0 +1,5 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const SHOWCOUNTRY = 'react-capstone-project/redux/countries';
+const URL = 'https://restcountries.com/v3.1/region/europe';
+
