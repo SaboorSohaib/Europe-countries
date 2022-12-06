c7c569b5f3126a2678830210d38872cfeae82d8d
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-12-06 21:11:17 +0430

    Add style for country details

--- src/components/country.js
+++ src/components/country.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import { fetchCountry } from '../redux/countries/fetchcountries';
+import '../App.css';
 
 const CountriesDetalis = () => {
   const country = useSelector((state) => state.countries, shallowEqual);
@@ -15,30 +16,29 @@ const CountriesDetalis = () => {
   const index = country.findIndex((item) => item.id === +id);
 
   return (
-    <div>
-      <div>
+    <div className="county-details">
+      <div className="flag-name">
         <img src={country[index].image} alt="Flag" />
         <h3>{country[index].name}</h3>
       </div>
 
       <div>
-        <span>Country Information</span>
+        <span className="country-info-title">Country Information</span>
       </div>
       <div>
-        <div>
+        <div className="info">
           <p>Capital:</p>
           <span>{country[index].capital}</span>
         </div>
-
-        <div>
+        <div className="info">
           <p>Population:</p>
           <span>{country[index].population}</span>
         </div>
-        <div>
+        <div className="info">
           <p>Timezone:</p>
           <span>{country[index].timezone}</span>
         </div>
-        <div>
+        <div className="info">
           <p>Subregion:</p>
           <span>{country[index].subregion}</span>
         </div>
