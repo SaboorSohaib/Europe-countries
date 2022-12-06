688cefb93f83503076152e14c76641bd1ba35d6c
Abdul Saboor Sohaib <abdulsaboorsoahib@gmail.com>
2022-12-06 19:51:29 +0430

    Add styling for map

--- src/components/home.js
+++ src/components/home.js
@@ -3,6 +3,7 @@ import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import { fetchCountry } from '../redux/countries/fetchcountries';
 import AllCountry from './allCountries';
 import europe from '../images/europe.png';
+import '../App.css';
 
 let retriveData = true;
 const HomePage = () => {
@@ -18,15 +19,15 @@ const HomePage = () => {
 
   return (
     <div>
-      <div>
+      <div className="map">
         <img src={europe} alt="eurpe map" />
         <div>
           <h3>Europe</h3>
           <p>1,400.8 Billion</p>
         </div>
       </div>
-      <div>
-        <h3>See All Countries</h3>
+      <div className="title-div">
+        <h3 className="title">See All Countries</h3>
       </div>
       <AllCountry data={country} />
     </div>
